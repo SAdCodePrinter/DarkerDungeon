a38e81d279c111f6615941d7f9bdf7cc45002e5b
PhilippsAir <philipp.bauer@std.dhsh.de>
2024-05-17 12:06:56 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/DarkerDungeonT/MainGUI/GamePanel.class
    #	out/production/DarkerDungeonT/entity/Entity.class
    #	out/production/DarkerDungeonT/entity/Player.class
    #	out/production/DarkerDungeonT/entity/Player2.class
    #	src/MainGUI/GamePanel.java
    #	src/entity/Entity.java
    #	src/entity/Player2.java

--- src/tile/TileHandler.java
+++ src/tile/TileHandler.java
@@ -7,19 +7,23 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class TileHandler {
 
     GamePanel gamePanel;
     Tile[] tile;
+    int mapTileNum[][];
 
     public TileHandler(GamePanel gamePanel){
 
         this.gamePanel = gamePanel;
         tile = new Tile [38];
+        mapTileNum = new int[gamePanel.getScreenWith()][gamePanel.getScreenHeight()];
         getTileImage();
+        loadMap();
 
-        //BufferedReader
     }
     private void getTileImage(){
         //Instanziieren der Tiles:
@@ -45,20 +49,57 @@ public class TileHandler {
             e.printStackTrace();
         }
     }
+    public void loadMap() {
+        try {
+            InputStream is = getClass().getResourceAsStream("/maps/SampleDungeon.rtf");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            int row = 0;
+            int col = 0;
+
+            // Lesen des Text Files:
+            while (col < gamePanel.getScreenWith() && row < gamePanel.getScreenHeight()) {
+
+                String line = br.readLine();
+
+                while (col < gamePanel.getScreenWith()){
+
+                    String numbers[] = line.split(" ");
+
+                    int num = Integer.parseInt(numbers[col]);
+
+                    mapTileNum[col][row] = num;
+                    col++;
+                }
+                if (col == gamePanel.getScreenWith()) {
+                    col = 0;
+                    row ++;
+                }
+            }
+
+            // Schließen des BufferedReader
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public void drawBackGroundTiles(Graphics2D g3){
         //g3.drawImage(tile[0].image, 0,0, gamePanel.getTileSize(), gamePanel.getTileSize(), null);
 
-        int mapRow = 0; //Breite
-        int mapCol = 0; //Höhe
+        int mapRow = 0; //Höhe
+        int mapCol = 0; //Breite
         int x = 0;
         int y = 0;
 
-        while (mapCol < gamePanel.getScreenHeight() && mapRow < gamePanel.getScreenWith()) {
-            g3.drawImage(tile[0].image,x ,y , gamePanel.getTileSize(), gamePanel.getTileSize(), null);
+        while (mapCol < gamePanel.getScreenWith() && mapRow < gamePanel.getScreenHeight()) {
+
+            int tileNum = mapTileNum[mapCol][mapRow];
+
+            g3.drawImage(tile[tileNum].image,x ,y , gamePanel.getTileSize(), gamePanel.getTileSize(), null);
             mapCol++;
             x += gamePanel.getTileSize();
 
-            if(mapCol == gamePanel.getScreenHeight()){
+            if(mapCol == gamePanel.getScreenWith()){
                 mapCol = 0;
                 x = 0;
                 mapRow++;
