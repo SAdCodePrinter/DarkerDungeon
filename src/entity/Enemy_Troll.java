5c32aa4d9f355af614d002bfcd38d05d79e8fefb
PhilippsAir <philipp.bauer@std.dhsh.de>
2024-05-22 14:53:57 +0200

    Troll

---
+++ src/entity/Enemy_Troll.java
@@ -0,0 +1,53 @@
+package entity;
+
+import MainGUI.GamePanel;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Enemy_Troll extends Entity{
+
+    public Enemy_Troll (GamePanel gamePanel, String imagePath){
+        super (gamePanel);
+        getTrollImage(imagePath);
+    }
+    public void setDefault(int xKoord, int yKoord, int defineSpeed) {
+        setX(xKoord);
+        setY(yKoord);
+        setSpeed(defineSpeed);
+        setDirection("down");
+    }
+    public void getTrollImage(String path) {
+        try {
+            for (int i = 0; i < 8; i++) {
+                String tmp = path + "down (" + (i + 1) + ").png";
+                //up[i] = ImageIO.read(Objects.requireNonNull(Player.class.getResourceAsStream(path + "up (" + (i + 1) + ").png")));
+                down[i] = ImageIO.read(Objects.requireNonNull(Player.class.getResourceAsStream(path + "down (" + (i + 1) + ").png")));
+                left[i] = ImageIO.read(Objects.requireNonNull(Player.class.getResourceAsStream(path + "left (" + (i + 1) + ").png")));
+                right[i] = ImageIO.read(Objects.requireNonNull(Player.class.getResourceAsStream(path + "right (" + (i + 1) + ").png")));
+                //idle[i] = ImageIO.read(Objects.requireNonNull(Player.class.getResourceAsStream(path + "idle (" + (i + 1) + ").png")));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void drawTroll(Graphics2D g) {
+        BufferedImage imageTroll =
+                switch (direction) {
+                    //case "idle" -> idle[spriteNum];
+                    //case "up" -> up[spriteNum];
+                    case "down" -> down[spriteNum];
+                    case "left" -> left[spriteNum];
+                    case "right" -> right[spriteNum];
+                    default -> null;
+                };
+
+        if (imageTroll != null) {
+            g.drawImage(imageTroll, getX(), getY(), gamePanel.getTileSize()*10, gamePanel.getTileSize()*10, null);
+        }
+    }
+}
